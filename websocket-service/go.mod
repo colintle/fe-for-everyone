662a4a2d857da7cc025231af34d27aeb5164f07e
Colin Le <Colin.Le@ucf.edu>
2024-04-19 22:14:51 -0400

    Added redis and connected it with the websocket & added blueprints for potential endpoints

--- websocket-service/go.mod
+++ websocket-service/go.mod
@@ -3,13 +3,12 @@ module websocket-service
 go 1.21.5
 
 require (
-	github.com/fatih/color v1.9.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/githubnemo/CompileDaemon v1.4.0 // indirect
-	github.com/gorilla/websocket v1.5.1 // indirect
-	github.com/mattn/go-colorable v0.1.4 // indirect
-	github.com/mattn/go-isatty v0.0.11 // indirect
-	github.com/radovskyb/watcher v1.0.7 // indirect
+	github.com/go-redis/redis/v8 v8.11.4
+	github.com/gorilla/websocket v1.5.1
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
 )
--- websocket-service/main.go
+++ websocket-service/main.go
@@ -1,43 +1,74 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/gorilla/websocket"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/websocket"
 )
 
+var ctx = context.Background()
+
+var rdb *redis.Client
 var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
-}
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-    ws, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer ws.Close()
-
-    for {
-        _, msg, err := ws.ReadMessage()
-        if err != nil {
-            log.Println("read:", err)
-            break
-        }
-        log.Printf("recv: %s", msg)
-        err = ws.WriteMessage(websocket.TextMessage, msg)
-        if err != nil {
-            log.Println("write:", err)
-            break
-        }
-    }
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Adjust to serve your needs
+	},
 }
 
 func main() {
-    http.HandleFunc("/ws", handleConnections)
-    log.Println("WebSocket service starting on :8081...")
-    err := http.ListenAndServe(":8081", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	// Initialize Redis client.
+	initializeRedis()
+
+	// Start WebSocket server.
+	http.HandleFunc("/ws", handleConnections)
+	log.Println("WebSocket server starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func initializeRedis() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // No password set by default
+		DB:       0,  // Use default DB
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Could not connect to Redis: %v", err)
+	}
+	fmt.Println("Connected to Redis")
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	// Run the reader in a separate goroutine so it doesn't block.
+	go readFromWebSocket(ws)
+}
+
+func readFromWebSocket(ws *websocket.Conn) {
+	for {
+		_, message, err := ws.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading from WebSocket: %v", err)
+			break
+		}
+
+		// Publish the message to the Redis "chat" channel
+		err = rdb.Publish(ctx, "chat", string(message)).Err()
+		if err != nil {
+			log.Printf("Error publishing to Redis: %v", err)
+			continue
+		}
+
+		log.Printf("Published to Redis: %s", message)
+	}
 }
