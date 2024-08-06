68f6df3cf59d4837f576f42430df88c1bf90ddf4
Colin Le <Colin.Le@ucf.edu>
2024-08-06 11:07:03 -0400

    Add restriction to app based on size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function App() {
+  const isMobile = window.matchMedia("only screen and (max-width: 1024px)").matches;
+
+  if(isMobile){
+    return <div>Sorry, this website is only available on desktop devices.</div>
+  }
   return (
     <div className="text-3xl font-bold underline">
         Hello
