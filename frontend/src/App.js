15f2280be5f18c69ee226f9afe85a3a9d0d597c4
Colin Le <Colin.Le@ucf.edu>
2024-08-07 09:38:31 -0400

    Add container under the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import Popup from './components/Popup';
 
 function App() {
   const isMobile = window.matchMedia("only screen and (max-width: 1024px)").matches;
@@ -8,8 +9,11 @@ function App() {
     return <div>Sorry, this website is only available on desktop devices.</div>
   }
   return (
-    <div>
-      <Navbar/>
+    // Parent Container
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-grow">
+      </div>
     </div>
   )
 }
