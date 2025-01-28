47a56deb57056da5205c187203e80f90f0bd42e4
Colin Le <Colin.Le@ucf.edu>
2025-01-28 17:55:25 -0500

    Close modal when click outside of it

--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Popup = ({ children }) => {
+const Popup = ({ children, onClose }) => {
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-11/12 max-w-lg h-fit max-h-screen overflow-auto">
+    <div className="fixed inset-0 z-10 flex items-center justify-center bg-gray-900 bg-opacity-50" onClick={handleOverlayClick}>
+      <div className="bg-white p-6 z-20 rounded-lg shadow-lg max-w-lg overflow-auto" onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
