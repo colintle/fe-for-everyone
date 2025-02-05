673f22745369fd9c82f81fcf1ec60843de71db27
Colin Le <Colin.Le@ucf.edu>
2025-02-05 18:50:35 -0500

    Return json for /health

--- backend/src/main/java/com/backend/backend/HealthCheck/HealthCheckController.java
+++ backend/src/main/java/com/backend/backend/HealthCheck/HealthCheckController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/health")
 public class HealthCheckController {
 
     @GetMapping
-    public String healthCheck() {
-        return "Application is healthy and running";
+    public Map<String, String> healthCheck() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "healthy");
+        response.put("message", "Application is running smoothly");
+        return response;
     }
 }
