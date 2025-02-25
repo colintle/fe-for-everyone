243354243e986439ac86c7aa4392b17f7688126f
Colin Le <Colin.Le@ucf.edu>
2025-02-25 15:30:19 -0500

    Add fetch completed problems to history

--- frontend/src/components/history/History.js
+++ frontend/src/components/history/History.js
@@ -1,12 +1,26 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { HiMiniArrowSmallLeft, HiMiniArrowSmallRight } from "react-icons/hi2";
 import { MyContext } from '../../MyProvider';
 
+import { useCodeApiCalls } from '../../utils/code/useCodeApiCalls';
+
 function History() {
-  const { completedProblems } = useContext(MyContext);
+  const { completedProblems, setCompletedProblems, accessToken } = useContext(MyContext);
+  const { getCompletedProblems } = useCodeApiCalls();
   const [searchTerm, setSearchTerm] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
+
+  useEffect(() => {
+    const fetchCompletedProblems = async () => {
+      const response = await getCompletedProblems();
+      if (response) {
+        setCompletedProblems(response.problems);
+      }
+    };
+
+    fetchCompletedProblems();
+  }, [accessToken, getCompletedProblems, setCompletedProblems]);
 
   const filteredProblems = completedProblems.filter(problem =>
     problem.problemStatementPath.toLowerCase().includes(searchTerm.toLowerCase()) ||
