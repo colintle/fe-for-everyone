398ef43bacee3fc93c795c8f99f3393cf113034c
Colin Le <Colin.Le@ucf.edu>
2025-02-25 15:29:29 -0500

    Add new method for getting all completed problems

--- frontend/src/utils/code/useCodeApiCalls.js
+++ frontend/src/utils/code/useCodeApiCalls.js
@@ -1,26 +1,36 @@
 import { useApi } from "../api/useApi"
+import { GET, POST } from "../api/methods"
 
 export const useCodeApiCalls = () => {
     const { callApi } = useApi()
 
     const completeProblem = async (problemStatementPath) => {
-        const response = await callApi(`/problem/complete`, 'POST', { problemStatementPath });
+        const response = await callApi(`/problem/complete`, POST, { problemStatementPath });
         if (response?.error) {
           return false;
         }
         return true
     };
     
-      const uncompleteProblem = async (problemStatementPath) => {
-        const response = await callApi(`/problem/remove`, 'POST', { problemStatementPath });
+    const uncompleteProblem = async (problemStatementPath) => {
+        const response = await callApi(`/problem/remove`, POST, { problemStatementPath });
         if (response?.error) {
           return false;
         }
         return true
     };
 
+    const getCompletedProblems = async () => {
+        const response = await callApi(`/problem/`, GET);
+        if (response?.error) {
+          return null;
+        }
+        return response;
+    }
+
     return {
         completeProblem,
-        uncompleteProblem
+        uncompleteProblem,
+        getCompletedProblems
     }
 }
