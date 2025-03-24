8044aa1f5d58dba924e41e7c9fa6c82069f453a5
Colin Le <Colin.Le@ucf.edu>
2025-03-24 18:42:37 -0400

    Add checker for origin to overcome the cors issue

--- websocket-service/instance/instance.go
+++ websocket-service/instance/instance.go
@@ -2,6 +2,8 @@ package instance
 
 import (
 	"sync"
+	"os"
+	"net/http"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
@@ -14,7 +16,11 @@ var (
 var upgrader = websocket.Upgrader{
     ReadBufferSize:  65536,
     WriteBufferSize: 65536,
+    CheckOrigin: func(r *http.Request) bool {
+        return r.Header.Get("Origin") == os.Getenv("FRONTEND_URL")
+    },
 }
+
 var connMutex = &sync.Mutex{}
 
 func GetConnMutex() *sync.Mutex {
