8ec0b9ba0b67bff91c33d57dfaaf3ce4a44226ee
Colin Le <Colin.Le@ucf.edu>
2025-03-24 18:41:51 -0400

    wait for socket instance to be initialize and then navigate to /code

--- frontend/src/components/menu/Home.js
+++ frontend/src/components/menu/Home.js
@@ -6,7 +6,6 @@ import Step2 from './Step2';
 import Step3 from './Step3';
 import ProgressBar from './ProgressBar';
 import { MyContext } from '../../MyProvider';
-
 import { useRoomApiCalls } from '../../utils/room/useRoomApiCalls';
 
 function Home() {
@@ -17,7 +16,7 @@ function Home() {
     exam: '',
     roomName: ''
   });
-  const {accessToken, setSingle, setMulti, setRoomData, setLoading} = useContext(MyContext);
+  const { accessToken, setSingle, setMulti, setRoomData, setLoading, socket } = useContext(MyContext);
   const navigate = useNavigate();
 
   const nextStep = () => setCurrentStep(currentStep + 1);
@@ -26,22 +25,22 @@ function Home() {
   const { isJoined, createRoom } = useRoomApiCalls();
 
   useEffect(() => {
-    setSingle(false)
-    setMulti(false)
-    setRoomData({})
-  }, [setSingle, setMulti, setRoomData])
+    setSingle(false);
+    setMulti(false);
+    setRoomData({});
+  }, [setSingle, setMulti, setRoomData]);
 
   useEffect(() => {
     const checkJoined = async () => {    
       const response = await isJoined();  
       if (response?.room) {
-        setMessage("Please leave the current room to join another room. Click the 'Join Room' button at top right corner.")
+        setMessage("Please leave the current room to join another room. Click the 'Join Room' button at top right corner.");
       }
-    }
+    };
     if (accessToken){
       checkJoined();
     }
-  }, [accessToken, isJoined])
+  }, [accessToken, isJoined]);
 
   const handleModeSelection = (data) => {
     setFormData((prevData) => ({
@@ -61,26 +60,32 @@ function Home() {
   };
 
   const handleSubmit = async () => {
-    if (formData.mode === "Single"){
-      setSingle(formData)
-      navigate("/code")
+    if (formData.mode === "Single") {
+      setSingle(formData);
+      navigate("/code");
     }
 
-    if (formData.mode === "Multi"){
-      setLoading(true)
+    if (formData.mode === "Multi") {
+      setLoading(true);
       const response = await createRoom(formData.roomName, formData.exam);
       if (response.error) {
         setMessage('Error creating the room. Please try again.');
-        setLoading(false)
+        setLoading(false);
         return;
       }
 
-      setRoomData(response)
-      setMulti(formData)
-      setLoading(false)
-      navigate("/code")
+      setRoomData(response);
+      setMulti(formData);
+      setLoading(false);
     }
   };
+
+  useEffect(() => {
+    if (formData.mode === "Multi" && socket) {
+      setLoading(false)
+      navigate("/code");
+    }
+  }, [socket, formData.mode, navigate, setLoading]);
 
   return (
     <div className="flex flex-col items-center pt-48 h-full">
@@ -91,7 +96,6 @@ function Home() {
         </div>
 
         {/* Step Content */}
-
         {message ? 
           <p className="text-red-500 text-sm mb-4">{message}</p> : 
           <div className="w-full">
